8142a988318459227ce918f43e6be186565a94fa
LusineKaryan <72447443+LusineKaryan@users.noreply.github.com>
2021-01-11 18:01:07 +0100

    test
    test

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css" />
+        <title>mypage</title>
+    </head>
+    <body>
+        <div id="bloc_page">
+            <!-- L'en-tête -->
+            <?php include("entete.php"); ?>
+            <!-- Le corps -->
+            <section>
+                <article>
+                    <p>Article </p>
+                </article>
+                <aside>
+                    <p>Aside </p>
+                </aside>
+            </section>
+            <!-- Le pied de page -->
+            <?php include("pied_de_page.php"); ?>
+        </div>
+    </body>
+    </html>
